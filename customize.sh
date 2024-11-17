35a65914a5febc8f1a4a998c8528390dc0ee919b
Sempr <iamsempr@gmail.com>
2024-11-17 22:09:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -18,7 +18,7 @@ mkdir -p `dirname $MODPATH$CFG`
 ui_print "- Migrating $CFG"
 $CMDPREFIX cp -af $CFG $MODPATH$CFG
 ui_print "- Starting modifiy"
-sed -i '/gChannelBondingMode24GHz=/d;/gChannelBondingMode5GHz=/d;/gForce1x1Exception=/d;/sae_enabled=/d;/BandCapability=/d;s/^END$/gChannelBondingMode24GHz=1\ngChannelBondingMode5GHz=1\ngForce1x1Exception=0\nsae_enabled=1\nBandCapability=0\nEND/g' $MODPATH$CFG
+sed -i '/gEnableDFSChnlScan=/d;/gAllowDFSChannelRoam=/d;s/^END$/gEnableDFSChnlScan=0\ngAllowDFSChannelRoam=0\nEND/g' $MODPATH$CFG
 }
 done
 [[ -z $CFG ]] && abort "- Installation FAILED. Your device didn't support WCNSS_qcom_cfg.ini." || { mkdir -p $MODPATH/system; mv -f $MODPATH/vendor $MODPATH/system/vendor; mv -f $MODPATH/product $MODPATH/system/product; mv -f $MODPATH/system_ext $MODPATH/system/system_ext;}
